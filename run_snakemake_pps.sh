a55ff80c5f26f62b105d5088e909aef09d9c5cd1
BenjaminPeter <bpeter@uchicago.edu>
2016-05-02 14:37:00 -0500

    merge

--- run_snakemake_pps.sh
+++ run_snakemake_pps.sh
@@ -1,1 +1,1 @@
-snakemake -j400 --cluster-config config/cluster.json --cluster "qsub -l h_vmem={cluster.mem} -o {cluster.output} -e {cluster.err} -cwd -V  -l h=!spudling9\&!spudling11\&!spudling12 -S /bin/bash" $*
+snakemake -j400 --cluster-config config/cluster.json --cluster "qsub -l h_vmem={cluster.mem} -o {cluster.output} -e {cluster.err} -cwd -V  -l h=!bigmem02\&!spudling11 -S /bin/bash" $*
