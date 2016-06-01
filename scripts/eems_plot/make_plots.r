51e34ca659ceb22074096c565193e42a4bd93759
Benjamin Peter <bpeter@uchicago.edu>
2016-06-01 18:19:42 -0500

    change eems plotting scripts to use meta data
    eems plotting scripts for migration, q and scatterplots now use abbreviations
    from pop_display

--- scripts/eems_plot/make_plots.r
+++ scripts/eems_plot/make_plots.r
@@ -10,7 +10,14 @@ mcmcpath <- sprintf('eemsout/%d/%s/', 0:(nruns-1), name)
 datapath <- sprintf('eems/%s-run0', name)
 plotpath <- sprintf('eemsout/%s_nruns%d', name, nruns)
 
+pop_display <- args[4]
+pop_geo <- args[5]
+indiv_label <- args[6]
+
 plot.all.posterior.stuff(mcmcpath=mcmcpath,
                          datapath=datapath,
                          plotpath=plotpath,
-                         grid=grid)
+                         grid=grid, 
+			 pop_display=pop_display,
+			 pop_geo=pop_geo,
+                         indiv_label=indiv_label)
